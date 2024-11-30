750b8f845d19943cdff354ba71ae3d5b2572fedf
Erik D. <62072464+erikd256@users.noreply.github.com>
2024-11-30 21:55:49 +0100

    update index.js

--- index.js
+++ index.js
@@ -205,7 +205,7 @@ function splitRowsByDate(rows) {
     resultArray.push(result);
   }
 
-  const upTime = count ? ((sum / count) * 100).toFixed(2) + "%" : "--%";
+  const upTime = count ? ((sum / count) * 100).toFixed(2) + "%" : "-∞%";
   dateValues.upTime = upTime;
   return dateValues;
 }
